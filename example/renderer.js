9e9e527ee379b321b168174322d960f6dbfab9af
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2019-12-25 15:01:38 +0100

    update

--- example/renderer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var __1 = require("..");
-new __1.Titlebar({
-    backgroundColor: __1.Color.fromHex('#37474f'),
-    icon: './images/icon.svg',
-    shadow: true
-});
